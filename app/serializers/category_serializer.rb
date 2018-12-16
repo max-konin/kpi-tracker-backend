b477d672aba44155b49f2f12cfc71ac31802843e
Max Konin <maxim21214@gmail.com>
2018-12-16 13:23:14 +0700

    added controller, routes and serializer

---
+++ app/serializers/category_serializer.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id                :bigint(8)        not null, primary key
+#  kpi_period        :integer
+#  kpi_quantity_goal :integer
+#  name              :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+class CategorySerializer < ActiveModel::Serializer
+  attributes :id, :name, :kpi_period, :kpi_quantity_goal
+end
