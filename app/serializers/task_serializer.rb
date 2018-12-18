06996fb25b4049c0167e2a883efd636f689e5183
Max Konin <maxim21214@gmail.com>
2018-12-18 16:14:58 +0700

    Bugfix. Fix for seeds, added anotations

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id               :bigint(8)        not null, primary key
+#  kpi_points       :integer
+#  notes            :text
+#  task_finished_at :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  category_id      :bigint(8)
+#  user_id          :bigint(8)
+#
+# Indexes
+#
+#  index_tasks_on_category_id  (category_id)
+#  index_tasks_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :kpi_points, :notes, :task_finished_at
 
